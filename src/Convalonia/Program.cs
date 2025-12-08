70494f263040acb13c18f86e275bd9cb3c072a75
박진호 <pjh3@paytalab.com>
2025-12-08 10:12:33 +0900

    Remove `ApplicationBase` classes for Avalonia, WPF, and Core libraries

--- src/Convalonia/Program.cs
+++ src/Convalonia/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia;
+using Avalonia.ReactiveUI;
 using System;
 
 namespace Convalonia;
@@ -17,5 +18,6 @@ class Program
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace();
+            .LogToTrace()
+            .UseReactiveUI();
 }
